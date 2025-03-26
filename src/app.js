633245d90ce9fb1b03a954b7a75659711d56613a
Nael <naeldiaz@proton.me>
2025-03-26 19:30:38 +0000

    probando el dom

--- src/app.js
+++ src/app.js
@@ -5,7 +5,65 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+
+let pronoum = ['the', 'our'];
+let adj = ['great', 'big'];
+let noum = ['jogger', 'racoon'];
+let extensions = [".com", ".es", ".net"];
+
+
+
+const joinWords2 = () => {
+  let dominios = [];
+  for (let e in pronoum) {
+    for (let i in adj) {
+      for (let a in noum) {
+        for (let ext in extensions) {
+          let dominio = pronoum[e] + adj[i] + noum[a] + extensions[ext];
+          dominios.push(dominio);
+        }
+      }
+    }
+  }
+  return dominios.join('\n');
+}
+
+
+
+
+
+
+
+
+/*const joinWords = () => {
+  let dominios = [];
+  for (let i = 0; i < pronoum.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noum.length; k++) {
+        for (let m = 0; m < extensions.length; m++) {
+          let dominio = pronoum[i] + adj[j] + noum[k] + extensions[m];
+          dominios.push(dominio);
+
+
+
+        }
+
+      }
+
+    }
+
+  }
+ 
+
+  return dominios.join('\n');
+
+}
+  */
+
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.getElementById("change").innerHTML = joinWords2('\n');
+  //console.log(joinWords());
+  console.log(joinWords2());
 };
